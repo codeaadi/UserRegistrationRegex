9fb2a0e9ba1a429b3449b332833143354c1c3506
codeaadi <adityadubey362@gmail.com>
2023-02-07 12:51:37 +0530

    Refactor: method to accept One Uppercase

---
+++ com/bridgelabz/regex/PasswordOneupperCase.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * User need to follow pre-defined Password Rules.
+ * Rule-2 : Should have atLeast 1 Upper Case.
+ */
+
+public class PasswordOneupperCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter password : ");
+        String password = scanner.next();
+        System.out.println(Pattern.matches("^(?=.*[A-Z]).{8,}$", password));
+    }
+}
