61ca5ccc5f41a4ac12e019df78e53b53014c469c
codeaadi <adityadubey362@gmail.com>
2023-02-07 12:42:39 +0530

    Added:Program to add email

---
+++ com/bridgelabz/regex/EnterEmail.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * User Need to Enter Valid Email
+ */
+
+public class EnterEmail {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String email = "abc.xyz@bl.co.in";
+        System.out.println("Email : " + email);
+        System.out.println(Pattern.matches("^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$", email));
+    }
+}
